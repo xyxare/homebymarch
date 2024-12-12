a33a561f6219571756c9f5aec4fd9dae84aacc05
ferjen <ferjendavetorred@gmail.com>
2024-12-12 11:49:32 +0800

    Add(Shop): improve shop flow

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -4,7 +4,7 @@ using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
+using System.IO;    
 
 [System.Serializable]
 public class PlayerData : MonoBehaviour
