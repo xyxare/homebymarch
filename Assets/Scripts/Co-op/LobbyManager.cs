049c7248e921f7b600ce1be2d68da961f80629e8
ferjen <ferjendavetorred@gmail.com>
2024-12-17 20:17:16 +0800

    Feat(Co-op): Added Maze

--- Assets/Scripts/Co-op/LobbyManager.cs
+++ Assets/Scripts/Co-op/LobbyManager.cs
@@ -175,7 +175,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 2)
+        if (PhotonNetwork.IsMasterClient)
         {
             playButton.SetActive(true);
         }
