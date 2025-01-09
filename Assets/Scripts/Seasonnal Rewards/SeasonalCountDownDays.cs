2d2dd8d5f7acbea07ad1ed985a7a63eca3fff3f4
ferjen <ferjendavetorred@gmail.com>
2025-01-09 12:06:04 +0800

    Update seasonal sword description and skill parameters

--- Assets/Scripts/Seasonnal Rewards/SeasonalCountDownDays.cs
+++ Assets/Scripts/Seasonnal Rewards/SeasonalCountDownDays.cs
@@ -7,8 +7,13 @@ public class SeasonalDaysCountDown : MonoBehaviour
     public TextMeshProUGUI countdownText; // Reference to the TextMeshProUGUI component
     public DateTime targetDate; // The target date for the countdown
 
+    public GameObject seasonpanel;
+
     void Start()
     {
+        // Set the target date to a specific date (e.g., December 31, 2023)
+        targetDate = new DateTime(2025, 02, 06);
+
         // Start the countdown immediately
         StartCoroutine(UpdateCountdown());
     }
@@ -17,19 +22,23 @@ public class SeasonalDaysCountDown : MonoBehaviour
     {
         while (true)
         {
-            DateTime targetDate = DateTime.Now.Date.AddDays(20);
-            TimeSpan timeRemaining = targetDate - DateTime.Now.Date;
+            TimeSpan timeRemaining = targetDate - DateTime.Now;
 
             if (timeRemaining.TotalDays <= 0)
             {
                 countdownText.text = "0 Days Left";
+                seasonpanel.SetActive(false);
                 yield break; // Stop the countdown when the target date is reached
             }
 
             // Format the remaining time as Days
-            countdownText.text = string.Format("{0} Days Left", timeRemaining.Days);
+            countdownText.text = string.Format("{0} Days Left", Mathf.CeilToInt((float)timeRemaining.TotalDays));
 
-            yield return new WaitForSeconds(86400); // Wait for 1 day (86400 seconds) before updating the time again
+            // Wait for 1 second and then loop to avoid time drift
+            for (int i = 0; i < 86400; i++)
+            {
+                yield return new WaitForSeconds(1);
+            }
         }
     }
 }
